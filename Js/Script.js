e44a2325f4abd69941f6c645d19b13630e30b8d3
FlorCollosso <florcollosso@gmail.com>
2022-05-11 18:39:29 -0300

    Cambios
    Cambios

--- Js/Script.js
+++ Js/Script.js
@@ -1,13 +1,17 @@
 
 
-
-
-
                               //Buscador de Contenido
 
   //Ejecutando Funciones
-
-document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
+var flag = true;
+document.getElementById("icon-search").addEventListener("click", () =>
+{
+    if(flag){
+      mostrar_buscador();
+    }else{
+      ocultar_buscador();
+    }
+});
 document.getElementById("ctn-cover-search").addEventListener("click", ocultar_buscador);
 
   //Declarando Variables
@@ -15,7 +19,7 @@ document.getElementById("ctn-cover-search").addEventListener("click", ocultar_bu
   bars_search =       document.getElementById("ctn-bars-search");
   cover_ctn_search =  document.getElementById("ctn-cover-search");
   input_search =      document.getElementById("InputSearch");
-  box_search =        document.getElementsById("BoxSearch");
+  box_search =        document.getElementById("BoxSearch");
 
 
    //Funcion para mostrar el buscador
@@ -24,6 +28,7 @@ document.getElementById("ctn-cover-search").addEventListener("click", ocultar_bu
      bars_search.style.top= "4vw";
      cover_ctn_search.style.display= "block";
      input_search.focus();
+     flag = false;
    }
 
    //Funcion para ocultar el Buscador
@@ -32,4 +37,5 @@ document.getElementById("ctn-cover-search").addEventListener("click", ocultar_bu
      bars_search.style.top= "-9vw";
      cover_ctn_search.style.display= "none";
      input_search.value= "";
+     flag = true;
    }
