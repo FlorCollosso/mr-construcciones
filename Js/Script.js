d14caea7ef4372b72598a7e13fa18050414e2112
FlorCollosso <86524914+FlorCollosso@users.noreply.github.com>
2022-07-06 23:51:13 -0300

    Mejora en la estructura HTML, animaciones nuevas con JS y librería AOS

--- Js/Script.js
+++ Js/Script.js
@@ -1,49 +1,60 @@
 
 
-                              //Buscador de Contenido
+//Buscador de Contenido
 
-  //Ejecutando Funciones
+//Ejecutando Funciones
 var flag = true;
-document.getElementById("icon-search").addEventListener("click", () =>
-{
-    if(flag){
-      mostrar_buscador();
-    }else{
-      ocultar_buscador();
-    }
+document.getElementById("icon-search").addEventListener("click", () => {
+  if (flag) {
+    mostrar_buscador();
+  } else {
+    ocultar_buscador();
+  }
 });
 document.getElementById("ctn-cover-search").addEventListener("click", ocultar_buscador);
 
-  //Declarando Variables
+//Declarando Variables
 
-  bars_search =       document.getElementById("ctn-bars-search");
-  cover_ctn_search =  document.getElementById("ctn-cover-search");
-  input_search =      document.getElementById("inputSearch");
-  box_search =        document.getElementById("boxSearch");
+bars_search = document.getElementById("ctn-bars-search");
+cover_ctn_search = document.getElementById("ctn-cover-search");
+input_search = document.getElementById("inputSearch");
+box_search = document.getElementById("boxSearch");
 
 
-   //Funcion para mostrar el buscador
+//Funcion para mostrar el buscador
 
-   function mostrar_buscador(){
-     bars_search.style.top= "4vw";
-     cover_ctn_search.style.display= "block";
-     input_search.focus();
-     flag = false;
-   }
+function mostrar_buscador() {
+  bars_search.style.top = "4vw";
+  cover_ctn_search.style.display = "block";
+  input_search.focus();
+  flag = false;
+}
 
-   //Funcion para ocultar el Buscador
+//Funcion para ocultar el Buscador
 
-   function ocultar_buscador(){
-     bars_search.style.top= "-9vw";
-     cover_ctn_search.style.display= "none";
-     input_search.value= "";
-     flag = true;
-   }
+function ocultar_buscador() {
+  bars_search.style.top = "-9vw";
+  cover_ctn_search.style.display = "none";
+  input_search.value = "";
+  flag = true;
+}
 
-                          //Animación Valores
-  console.log(document.getElementById("divRapidez"))
-  document.getElementById("divRapidez").addEventListener("mouseenter", () => {
-    document.getElementById("divRapidez").classList.remove("animacion");
-    console.log("entre")
-  });
-  
+//Animación Valores
+
+document.getElementById("divRapidez").addEventListener("mouseenter", () => {
+  document.getElementById("divRapidez").classList.remove("animacion");
+
+});
+
+
+let hrAnimado = document.getElementById("hrAnimacion");
+
+function mostrarScroll() {
+  let scrollTop = document.documentElement.scrollTop;
+  let alturaHrAnimado = hrAnimado.offsetTop;
+  if (alturaHrAnimado + 200 < scrollTop) {
+    hrAnimado.classList.add("hrAnimacion")
+  }
+}
+
+window.addEventListener('scroll', mostrarScroll);
