ccb6d163b826bc856634ba93175fb0198a0b4147
FlorCollosso <86524914+FlorCollosso@users.noreply.github.com>
2022-07-12 01:06:57 -0300

    Funcionalidad del buscador agregada y corrección de errores

--- Js/Script.js
+++ Js/Script.js
@@ -28,6 +28,10 @@ function mostrar_buscador() {
   cover_ctn_search.style.display = "block";
   input_search.focus();
   flag = false;
+  
+  if (input_search.value === "") {
+    box_search.style.display = "none"
+  }
 }
 
 //Función para ocultar el Buscador
@@ -37,6 +41,36 @@ function ocultar_buscador() {
   cover_ctn_search.style.display = "none";
   input_search.value = "";
   flag = true;
+  box_search.style.display = "none";
+}
+
+// Creando filtrado de búsqueda
+
+document.getElementById("inputSearch").addEventListener("keyup", buscadorInterno);
+
+function buscadorInterno() {
+
+  filter = input_search.value.toUpperCase();
+  li = box_search.getElementsByTagName("li");
+
+  //Recorriendo elementos a filtrar mediante los "li"
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    textValue = a.textContent || a.innerText;
+
+    if(textValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+      box_search.style.display = "block";
+
+      if (input_search.value === "") {
+        box_search.style.display = "none"
+      }
+
+    }else {
+      li[i].style.display = "none";
+    }
+  }
 }
 
 //Animación de la seección Valores
